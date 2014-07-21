1a291f61b5c18b803a74efa42c3bd077e8706d44
fabrizioballiano <fabrizioballiano@caolila.dreamhost.com>
2014-07-21 14:09:13 -0700

    complite rebuild

--- src/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf bower_components
-rm -rf components
-php generate.php>generate.html
-mv bower_components components
-vulcanize --strip --inline generate.html --output polymer-full.html
