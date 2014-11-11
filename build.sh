286574241bb1949909db744cc01c1b3596768a60
fabrizioballiano <fabrizioballiano@caolila.dreamhost.com>
2014-11-11 00:56:37 -0800

    0.5.0

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 vulcanize --strip --inline polymer-full-src.html --output polymer-full.html
-cp ./bower_components/platform/platform.js .
-cp ./bower_components/platform/platform.js.map .
+#cp ./bower_components/platform/platform.js .
+#cp ./bower_components/platform/platform.js.map .
+cp bower_components/webcomponentsjs/webcomponents.min.js .
